e35e1d0f5c973b3da55060399c6569a815fccae0
Marcus-KEA <mahe0009@stud.kea.dk>
2026-01-05 23:29:37 +0100

    Change age from long to Long wrapper class

--- src/main/java/dk/ek/examdemo/model/Student.java
+++ src/main/java/dk/ek/examdemo/model/Student.java
@@ -16,9 +16,8 @@ import lombok.NoArgsConstructor;
 public class Student {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String age;
-    private String subject;
 }
